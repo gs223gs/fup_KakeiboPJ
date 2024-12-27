6a36cf070c25f4d262bce49550f58fe9ca041cd3
T.Miura <144312244+gs223gs@users.noreply.github.com>
2024-12-27 16:33:44 +0900

    refactor: ヘッダーとフッターのファイルを移動　接続確認コードを追加

---
+++ includes/view/footer.php
@@ -0,0 +1,10 @@
+<?php
+// includes/view/footer.php
+?>
+
+</b>
+</html>
+<footer>
+    <p>フッター</p>
+    <p>ここを表示しているファイル名:includes/view/footer.php</p>
+</footer>
---
+++ includes/view/header.php
@@ -0,0 +1,14 @@
+<?php
+// includes/header.php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>後で変更</title>
+</head>
+<body>
+  <p>ここを表示しているファイル名:includes/view/header.php</p>
