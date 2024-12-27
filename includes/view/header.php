860837564b2d7e067a7a1904b0aedb3a598fec8d
T.Miura <144312244+gs223gs@users.noreply.github.com>
2024-12-28 00:49:06 +0900

    refactor: CSS読み込み関数をheader.phpに組み込み

--- includes/view/header.php
+++ includes/view/header.php
@@ -5,6 +5,19 @@
 
 // 読み込みたいCSSファイルを配列で指定
 // $css_files = ['style1.css', 'style2.css'];
+
+
+
+?>
+<?php
+// includes/view/header.php
+// $pagetitleは書くページで宣言する
+// cssの読み込み専用の関数　複数ある場合は、複数読み込む
+function read_css_files($css_files) {
+    foreach ($css_files as $file) {
+        echo '<link rel="stylesheet" type="text/css" href="' . $file . '">';
+    }
+}
 ?>
 
 <!-- このヘッダーのmetaは適当に作成している -->
