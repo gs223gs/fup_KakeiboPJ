afcaa52f01f19f82892007bd9ef69899d2cb54fe
T.Miura <144312244+gs223gs@users.noreply.github.com>
2024-12-28 00:47:11 +0900

    refactor: CSS読み込み関数をheader.phpに組み込んだため，includeの削除

--- view/auth/login.php
+++ view/auth/login.php
@@ -4,11 +4,14 @@ $css_files = ['../../assets/css/header.css', '../../assets/css/footer.css'];
 
 require_once __DIR__ . '/../../config/config.php'; // config.phpを読み込む
 //css読み込みファイルとheader読み込みファイルを定義
-require_once LOGIC_PATH . '/css/read_css.php';
 require_once INCLUDES_PATH . '/view/header.php';
+
+require_once INCLUDES_PATH . '/DB/db.php';
+connectDB();
 ?>
 <br>
 <!-- ユーザー登録画面への遷移 -->
+<a href ="../../">indexへ戻る</a><br>
 <a href="register.php">ユーザー登録</a>
 
 <?php
--- view/auth/register.php
+++ view/auth/register.php
@@ -1,14 +1,17 @@
 <?php
+//ページタイトルを設定
 $pagetitle = 'ログインページ';
+//読み込むCSSを設定
 $css_files = ['../../assets/css/header.css', '../../assets/css/footer.css'];
 
 require_once __DIR__ . '/../../config/config.php'; // config.phpを読み込む
 //css読み込みファイルとheader読み込みファイルを定義
-require_once LOGIC_PATH . '/css/read_css.php';
 require_once INCLUDES_PATH . '/view/header.php';
 ?>
 <br>
 <!-- ユーザー登録画面への遷移 -->
+
+<a href ="../../">indexへ戻る</a><br>
 <a href="login.php">ログイン</a>
 
 <?php
