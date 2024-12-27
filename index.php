64c049dcee54b775f3a30ced17c070eaeef18e42
T.Miura <144312244+gs223gs@users.noreply.github.com>
2024-12-27 16:34:01 +0900

    feat: ヘッダーとフッターをインクルードして接続確認

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<?php
+include 'includes/view/header.php';
+?>
+
 <!-- ログイン画面への遷移 -->
 <a href="./view/auth/login.php">ログイン</a>
 <br>
 <!-- ユーザー登録画面への遷移 -->
 <a href="./view/auth/register.php">ユーザー登録</a>
+
+<?php
+include 'includes/view/footer.php';
+?>
