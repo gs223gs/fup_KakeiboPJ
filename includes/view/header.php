95d85daf537b161c47f9f457e393e7522e1fa3b0
T.Miura <144312244+gs223gs@users.noreply.github.com>
2024-12-27 17:18:52 +0900

    feat: cssを読み込む関数を適用 関数呼び出しでCSS読み込みを実現

--- includes/view/header.php
+++ includes/view/header.php
@@ -1,6 +1,13 @@
 <?php
-// includes/header.php
+// includes/view/header.php
+// $pagetitleは書くページで宣言する
+// cssの読み込み専用の関数　複数ある場合は、複数読み込む
 
+// logic/css/read_css.phpを読み込む
+include './logic/css/read_css.php';
+
+// 読み込みたいCSSファイルを配列で指定
+// $css_files = ['style1.css', 'style2.css'];
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +15,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>後で変更</title>
+  <title><?php echo $pagetitle; ?></title>
+  <?php
+  // ここでCSSファイルを読み込む
+  read_css_files($css_files);
+  ?>
 </head>
 <body>
   <p>ここを表示しているファイル名:includes/view/header.php</p>
+  <p><?php echo $pagetitle; ?></p>
