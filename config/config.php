eb7854e164babb9d1024f13c03657053492178eb
T.Miura <144312244+gs223gs@users.noreply.github.com>
2024-12-28 00:48:41 +0900

    feat: assets PATHを追加

--- config/config.php
+++ config/config.php
@@ -6,12 +6,17 @@
 //DB接続用のファイル
 
 // リポジトリのルートディレクトリを基準にする
+// BASE_PATH = /fup_kakeibopj/ となる
 define('BASE_PATH', dirname(__DIR__));
 
 //リポジトリから見た絶対パスを定義
+//BASE_PATHと組み合わせると以下になる
+//fup_kakeibopj/includes/fizz
+//　つまり，どこからファイルパスを指定しようとしても，fup_kakeibopj/ スタートになるので，ファイルパスの指定が鬼ほど楽になります．
 define('INCLUDES_PATH', BASE_PATH . '/includes');
 define('LOGIC_PATH', BASE_PATH . '/logic');
 define('DB_PATH', BASE_PATH . '/DB');
 define('LOGS_PATH', BASE_PATH . '/logs');
+define('ASSETS_PATH', BASE_PATH . '/assets');
 ?>
 
